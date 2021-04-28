333197250b7a37fb29cff45df23d68310075fb18
Joyce Luong 🦈 <joycesluong@gmail.com>
2021-04-27 20:45:20 -0700

    successfully pushes data to and from aws lambda

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -5,23 +5,35 @@ import { Character } from '../modules/character';
 import { ItemData, SubType } from '../modules/data';
 import { Body, Item } from '../modules/item';
 import { Image } from './Image';
+import { getImagePayload } from '../modules/api';
+import { ItemId } from '../modules/data';
 
 export const Phone = (): JSX.Element => {
-  const itemsData = useSelector((state: RootState) => state.data.itemsData);
   const characterData = useSelector((state: RootState) => state.character.history[state.character.step]);
-  const hiddenList = useSelector((state: RootState) => state.editor.hiddenItems);
-  const character = new Character(characterData);
+  const clothes = new Character(characterData).clothes;
+  const [image, setImage] = useState('assets/fairytale.jpeg');
+
+  const parsePayload = (response: any) => {
+    setImage(JSON.parse(response.Payload).url);
+  };
+  
+  useEffect(() => {
+    const itemIds: ItemId[] = Object.values(clothes);
+    const response = getImagePayload(itemIds, parsePayload);
+  }, [characterData])
 
   return (
     <div id="phone-image">
       <div id="phone-menu">
-        <p>View Full Image</p>
-        <p>Download Image</p>
-        <p>Change background</p>
-        <p>Save to your outfits</p>
-        <p>View Full Screen</p>
+        <p>View Full Image</p><br></br>
+        <p>Download Image</p><br></br>
+        <p>Change background</p><br></br>
+        <p>Generate Link</p><br></br>
+        <p>View Full Screen</p><br></br>
+        <p>Toggle On Autorender Image</p><br></br>
+        <p>Render Image</p><br></br>
       </div>
-      <img src='assets/fairytale.jpeg' draggable="false" />
+      <img src={image} draggable="false" />
     </div>
   );
 };

dda438636dd03c85aa4b418aa2bc17211f139d3e
Joyce Luong 🦈 <joycesluong@gmail.com>
2021-05-31 19:28:43 -0700

    update to use icons from remote server

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './sprites.css';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
